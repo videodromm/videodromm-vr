9f407e1e98f7f3eb49f81c30413ec64293fcb1bb
Bruce LANE <bruce@batchass.fr>
2016-09-11 10:53:09 +0200

    Integrating Teleport sample from Cinder-VR block

---
+++ src/VideodrommVRApp.cpp
@@ -0,0 +1,631 @@
+#include "cinder/app/App.h"
+#include "cinder/app/RendererGl.h"
+#include "cinder/gl/gl.h"
+#include "cinder/AxisAlignedBox.h"
+#include "cinder/Log.h"
+#include "cinder/Rand.h"
+#include "cinder/Sphere.h"
+#include "cinder/Timeline.h"
+
+#include "cinder/vr/vr.h"
+// Settings
+#include "VDSettings.h"
+// Session
+#include "VDSession.h"
+// Animation
+#include "VDAnimation.h"
+
+#include "VDTexture.h"
+
+using namespace ci;
+using namespace ci::app;
+using namespace std;
+using namespace VideoDromm;
+
+class UiIntersectable;
+using UiIntersectablePtr = std::shared_ptr<UiIntersectable>;
+
+//! \class UiIntersectable
+//!
+//!
+class UiIntersectable {
+public:
+
+	virtual bool intersects(const ci::Ray &ray) const = 0;
+	virtual void draw() = 0;
+
+	bool getFocused() const {
+		return mFocused;
+	}
+
+	void setFocused(bool value) {
+		if (value != mFocused) {
+			mFocused = value;
+			float target = (mFocused ? 1.0f : 0.0f);
+			ci::app::App::get()->timeline().apply(&mFocusedValue, target, 0.25f);
+		}
+	}
+
+	bool getSelected() const {
+		return mSelected;
+	}
+
+	virtual void setSelected(bool value) {
+		if (value != mSelected) {
+			mSelected = value;
+			float target = (mSelected ? 1.0f : 0.0f);
+			ci::app::App::get()->timeline().apply(&mSelectedValue, target, 0.25f);
+		}
+	}
+
+	ColorA getColor() const {
+		float t = mSelectedValue.value();
+		Color color = (1.0f - t)*Color(0.4f, 0.4f, 0.42f) + t*Color(0.95f, 0.5f, 0.0f);
+		t = mFocusedValue.value();
+		color = (1.0f - t)*color + t*Color(0.9f, 0.9f, 0.0f);
+		return ColorA(color);
+	}
+
+	const ci::vec3& getPosition() const {
+		return mPosition;
+	}
+
+protected:
+	gl::GlslProgRef		mShader;
+	gl::BatchRef		mBatch;
+	bool				mFocused = false;
+	ci::Anim<float>		mFocusedValue = 0.0f;
+	bool				mSelected = false;
+	ci::Anim<float>		mSelectedValue = 0.0f;
+	ci::vec3			mPosition;
+};
+
+//! \class UiBox
+//!
+//!
+class UiBox : public UiIntersectable {
+public:
+	UiBox() {}
+
+	UiBox(const vec3 &center, const vec3 &size, const gl::GlslProgRef &shader) {
+		vec3 min = center - 0.5f*size;
+		vec3 max = center + 0.5f*size;
+		mBox = AxisAlignedBox(min, max);
+		mShader = shader;
+		mBatch = gl::Batch::create(geom::Cube().size(size) >> geom::Translate(center), shader);
+		mPosition = center;
+	}
+
+	virtual bool intersects(const ci::Ray &ray) const override {
+		bool result = false;
+		float t0 = std::numeric_limits<float>::min();
+		float t1 = std::numeric_limits<float>::min();
+		if (mBox.intersect(ray, &t0, &t1) && (t0 > 0.0f)) {
+			result = true;
+		}
+		return result;
+	}
+
+	virtual void draw() {
+		gl::ScopedDepth scopedDepth(false);
+
+		gl::ScopedBlendAlpha scopeBlend;
+		gl::ScopedColor scopedColor(getColor());
+		mBatch->draw();
+	}
+protected:
+	AxisAlignedBox mBox;
+};
+
+
+class VideodrommVRApp : public App {
+
+public:
+	void						setup() override;
+	void						keyDown(KeyEvent event);
+	void						mouseDown(MouseEvent event) override;
+	void						mouseDrag(MouseEvent event) override;
+	void						update() override;
+	void						draw() override;
+	void						fileDrop(FileDropEvent event) override;
+	void						cleanup() override;
+private:
+	// Settings
+	VDSettingsRef				mVDSettings;
+	// Session
+	VDSessionRef				mVDSession;
+	// Log
+	VDLogRef					mVDLog;
+	// Animation
+	VDAnimationRef				mVDAnimation;
+
+	VDTextureList				mTexs;
+	fs::path					mTexturesFilepath;
+	int							i, x;
+	// Cinder VR
+	ci::vr::Context				*mVrContext = nullptr;
+	ci::vr::Hmd					*mHmd = nullptr;
+	bool						mCyclopsMirroring = false;
+	CameraPersp					mCamera;
+
+	bool						mRecalcOrigin = false;
+
+	const ci::vr::Controller	*mController1 = nullptr;
+	const ci::vr::Controller	*mController2 = nullptr;
+
+	gl::BatchRef				mGridBox;
+	gl::BatchRef				mGridLines;
+
+	std::vector<UiIntersectablePtr>	mShapes;
+	uint32_t						mShapeIndex = 0;
+
+	void	onControllerConnect(const ci::vr::Controller *controller);
+	void	onControllerDisconnect(const ci::vr::Controller *controller);
+	void	onButtonDown(const ci::vr::Controller::Button *button);
+	void	onButtonUp(const ci::vr::Controller::Button *button);
+
+	void	initGrid(const gl::GlslProgRef &shader);
+	void	initShapes(const gl::GlslProgRef &shader);
+
+	void	drawScene();
+};
+void VideodrommVRApp::initGrid(const gl::GlslProgRef &shader)
+{
+	vec3 size = vec3(100, 20, 100);
+
+	// Box
+	{
+		mGridBox = gl::Batch::create(geom::Cube().size(1.01f*size) >> geom::Translate(0.0f, 0.0f, 0.0f), shader);
+	}
+
+	// Lines
+	{
+		Rectf bounds = Rectf(-size.x / 2.0f, -size.z / 2.0f, size.x / 2.0f, size.z / 2.0f);
+		int nx = static_cast<int>(size.x);
+		int nz = static_cast<int>(size.z);
+		float dx = bounds.getWidth() / static_cast<float>(nx);
+		float dz = bounds.getHeight() / static_cast<float>(nz);
+		float y = 0.01f;
+
+		std::vector<vec3> vertices;
+
+		for (int i = 0; i <= nx; ++i) {
+			float x = bounds.x1 + i * dx;
+			vec3 p0 = vec3(x, y, bounds.y1);
+			vec3 p1 = vec3(x, y, bounds.y2);
+			vertices.push_back(p0);
+			vertices.push_back(p1);
+		}
+
+		for (int i = 0; i <= nz; ++i) {
+			float z = bounds.y1 + i * dz;
+			vec3 p0 = vec3(bounds.x1, y, z);
+			vec3 p1 = vec3(bounds.x2, y, z);
+			vertices.push_back(p0);
+			vertices.push_back(p1);
+		}
+
+		gl::VboRef vbo = gl::Vbo::create(GL_ARRAY_BUFFER, vertices);
+		geom::BufferLayout layout = geom::BufferLayout({ geom::AttribInfo(geom::Attrib::POSITION, 3, sizeof(vec3), 0) });
+		gl::VboMeshRef vboMesh = gl::VboMesh::create(static_cast<uint32_t>(vertices.size()), GL_LINES, { std::make_pair(layout, vbo) });
+		mGridLines = gl::Batch::create(vboMesh, shader);
+	}
+}
+
+void VideodrommVRApp::initShapes(const gl::GlslProgRef &shader)
+{
+	mShapes.push_back(UiIntersectablePtr(new UiBox(vec3(0, -0.001f, 0) + vec3(0, -0.0625f, 3), vec3(4, 0.125f, 4), shader)));
+	mShapes.push_back(UiIntersectablePtr(new UiBox(vec3(0, -0.001f, 0) + vec3(-4, -0.0625f, -4), vec3(4, 0.125f, 4), shader)));
+	mShapes.push_back(UiIntersectablePtr(new UiBox(vec3(0, -0.001f, 0) + vec3(4, -0.0625f, -4), vec3(4, 0.125f, 4), shader)));
+	mShapes.push_back(UiIntersectablePtr(new UiBox(vec3(0, -0.001f, 0) + vec3(0, -0.0625f, -10), vec3(4, 0.125f, 4), shader)));
+	mShapes.push_back(UiIntersectablePtr(new UiBox(vec3(0, -0.001f, 0) + vec3(-4, -0.0625f, -4) + vec3(0, 5, -12), vec3(4, 0.125f, 4), shader)));
+	mShapes.push_back(UiIntersectablePtr(new UiBox(vec3(0, -0.001f, 0) + vec3(4, -0.0625f, -4) + vec3(0, 8, -12), vec3(4, 0.125f, 4), shader)));
+	mShapes.push_back(UiIntersectablePtr(new UiBox(vec3(0, -0.001f, 0) + vec3(-8, -0.0625f, 3), vec3(4, 0.125f, 4), shader)));
+	mShapes.push_back(UiIntersectablePtr(new UiBox(vec3(0, -0.001f, 0) + vec3(8, -0.0625f, 3), vec3(4, 0.125f, 4), shader)));
+	mShapes.push_back(UiIntersectablePtr(new UiBox(vec3(0, -0.001f, 0) + vec3(-16, -0.0625f, 3) + vec3(0, 4, 0), vec3(4, 0.125f, 4), shader)));
+	mShapes.push_back(UiIntersectablePtr(new UiBox(vec3(0, -0.001f, 0) + vec3(16, -0.0625f, 3) + vec3(0, 4, 0), vec3(4, 0.125f, 4), shader)));
+	mShapes.push_back(UiIntersectablePtr(new UiBox(vec3(0, -0.001f, 0) + vec3(-16, -0.0625f, 3) + vec3(0, 6.0f, -9), vec3(4, 0.125f, 4), shader)));
+	mShapes.push_back(UiIntersectablePtr(new UiBox(vec3(0, -0.001f, 0) + vec3(16, -0.0625f, 3) + vec3(0, 2.5f, -9), vec3(4, 0.125f, 4), shader)));
+
+	mShapes.push_back(UiIntersectablePtr(new UiBox(vec3(0, -0.001f, 0) + vec3(-4, -0.0625f, 4) + vec3(0, 8, 12), vec3(4, 0.125f, 4), shader)));
+	mShapes.push_back(UiIntersectablePtr(new UiBox(vec3(0, -0.001f, 0) + vec3(4, -0.0625f, 4) + vec3(0, 5, 12), vec3(4, 0.125f, 4), shader)));
+}
+
+
+void VideodrommVRApp::setup()
+{
+	// Settings
+	mVDSettings = VDSettings::create();
+	// Session
+	mVDSession = VDSession::create(mVDSettings);
+	// Animation
+	mVDAnimation = VDAnimation::create(mVDSettings, mVDSession);
+	// initialize 
+#if (defined(  CINDER_MSW) )
+	mTexturesFilepath = getAssetPath("") / "defaulttextures.xml";
+#else
+	mTexturesFilepath = getAssetPath("") / "defaulttexturesquicktime.xml";
+#endif
+	if (fs::exists(mTexturesFilepath)) {
+		// load textures from file if one exists
+		mTexs = VDTexture::readSettings(mVDAnimation, loadFile(mTexturesFilepath));
+	}
+	else {
+		// otherwise create a texture from scratch
+		mTexs.push_back(TextureAudio::create(mVDAnimation));
+	}
+	// Cinder VR
+	mCamera.lookAt(vec3(0, 0, 3), vec3(0, 0, 0));
+
+	gl::disableAlphaBlending();
+	gl::enableDepthRead();
+	gl::enableDepthWrite();
+	gl::color(Color::white());
+
+	try {
+		ci::vr::initialize();
+	}
+	catch (const std::exception& e) {
+		CI_LOG_E("VR failed: " << e.what());
+	}
+
+	try {
+		mVrContext = ci::vr::beginSession(
+			ci::vr::SessionOptions()
+			.setTrackingOrigin(ci::vr::TRACKING_ORIGIN_SEATED)
+			.setOriginOffset(vec3(0, -1, -3))
+			.setControllersScanInterval(0.25f)
+			.setControllerConnected(std::bind(&VideodrommVRApp::onControllerConnect, this, std::placeholders::_1))
+			.setControllerDisconnected(std::bind(&VideodrommVRApp::onControllerDisconnect, this, std::placeholders::_1))
+			);
+	}
+	catch (const std::exception& e) {
+		CI_LOG_E("Session failed: " << e.what());
+	}
+
+	try {
+		mHmd = mVrContext->getHmd();
+	}
+	catch (const std::exception& e) {
+		CI_LOG_E("Hmd failed: " << e.what());
+	}
+
+	mVrContext->getSignalControllerButtonDown().connect(std::bind(&VideodrommVRApp::onButtonDown, this, std::placeholders::_1));
+	mVrContext->getSignalControllerButtonUp().connect(std::bind(&VideodrommVRApp::onButtonUp, this, std::placeholders::_1));
+
+	auto shader = gl::getStockShader(gl::ShaderDef().color());
+	initGrid(shader);
+	initShapes(shader);
+}
+void VideodrommVRApp::keyDown(KeyEvent event)
+{
+	switch (event.getChar()) {
+	case '1': {
+		mCyclopsMirroring = false;
+		if (mHmd) {
+			mHmd->setMirrorMode(ci::vr::Hmd::MirrorMode::MIRROR_MODE_STEREO);
+		}
+	}
+			  break;
+
+	case '2': {
+		mCyclopsMirroring = false;
+		if (mHmd) {
+			mHmd->setMirrorMode(ci::vr::Hmd::MirrorMode::MIRROR_MODE_UNDISTORTED_STEREO);
+		}
+	}
+			  break;
+
+	case '3': {
+		mCyclopsMirroring = false;
+		if (mHmd) {
+			mHmd->setMirrorMode(ci::vr::Hmd::MirrorMode::MIRROR_MODE_UNDISTORTED_MONO_LEFT);
+		}
+	}
+			  break;
+
+	case '4': {
+		mCyclopsMirroring = false;
+		if (mHmd) {
+			mHmd->setMirrorMode(ci::vr::Hmd::MirrorMode::MIRROR_MODE_UNDISTORTED_MONO_RIGHT);
+		}
+	}
+			  break;
+
+	case '5': {
+		mCyclopsMirroring = true;
+		if (mHmd) {
+			mHmd->setMirrorMode(ci::vr::Hmd::MirrorMode::MIRROR_MODE_NONE);
+		}
+	}
+			  break;
+	}
+}
+void VideodrommVRApp::fileDrop(FileDropEvent event)
+{
+	int index = 1;
+	string ext = "";
+	// use the last of the dropped files
+	boost::filesystem::path mPath = event.getFile(event.getNumFiles() - 1);
+	string mFile = mPath.string();
+	int dotIndex = mFile.find_last_of(".");
+	int slashIndex = mFile.find_last_of("\\");
+	bool found = false;
+
+	if (dotIndex != std::string::npos && dotIndex > slashIndex) ext = mFile.substr(mFile.find_last_of(".") + 1);
+
+	if (ext == "wav" || ext == "mp3")
+	{
+		for (auto tex : mTexs)
+		{
+			if (!found) {
+				if (tex->getType() == VDTexture::AUDIO) {
+					tex->loadFromFullPath(mFile);
+					found = true;
+				}
+			}
+		}
+	}
+	else if (ext == "png" || ext == "jpg")
+	{
+		for (auto tex : mTexs)
+		{
+			if (!found) {
+				if (tex->getType() == VDTexture::IMAGE) {
+					tex->loadFromFullPath(mFile);
+					found = true;
+				}
+			}
+		}
+	}
+	else if (ext == "mov")
+	{
+		for (auto tex : mTexs)
+		{
+			if (!found) {
+				if (tex->getType() == VDTexture::MOVIE) {
+					tex->loadFromFullPath(mFile);
+					found = true;
+				}
+			}
+		}
+	}
+	else if (ext == "")
+	{
+		// try loading image sequence from dir
+		for (auto tex : mTexs)
+		{
+			if (!found) {
+				if (tex->getType() == VDTexture::SEQUENCE) {
+					tex->loadFromFullPath(mFile);
+					found = true;
+				}
+			}
+		}
+	}
+
+}
+
+void VideodrommVRApp::onControllerConnect(const ci::vr::Controller* controller)
+{
+	if (!controller) {
+		return;
+	}
+
+	bool connected = false;
+	if (ci::vr::API_OCULUS == controller->getApi()) {
+	}
+	else if (ci::vr::API_OPENVR == controller->getApi()) {
+		switch (controller->getType()) {
+		case ci::vr::Controller::TYPE_LEFT: {
+			mController1 = controller;
+			connected = true;
+		}
+											break;
+
+		case ci::vr::Controller::TYPE_RIGHT: {
+			mController2 = controller;
+			connected = true;
+		}
+											 break;
+		}
+	}
+
+	if (connected) {
+		CI_LOG_I("Controller connected: " << controller->getName());
+	}
+}
+
+void VideodrommVRApp::onControllerDisconnect(const ci::vr::Controller* controller)
+{
+	if (!controller) {
+		return;
+	}
+
+	bool disconnected = false;
+	if (ci::vr::API_OCULUS == controller->getApi()) {
+	}
+	else if (ci::vr::API_OPENVR == controller->getApi()) {
+		switch (controller->getType()) {
+		case ci::vr::Controller::TYPE_LEFT: {
+			mController1 = nullptr;
+			disconnected = true;
+		}
+											break;
+
+		case ci::vr::Controller::TYPE_RIGHT: {
+			mController2 = nullptr;
+			disconnected = true;
+		}
+											 break;
+		}
+	}
+
+	if (disconnected) {
+		CI_LOG_I("Controller disconnected: " << controller->getName());
+	}
+}
+
+void VideodrommVRApp::onButtonDown(const ci::vr::Controller::Button *button)
+{
+	uint32_t shapdeIndex = mShapeIndex;
+	for (uint32_t i = 0; i < mShapes.size(); ++i) {
+		auto& shape = mShapes[i];
+		if (shape->intersects(mHmd->getInputRay())) {
+			shapdeIndex = i;
+			break;
+		}
+	}
+
+	if (shapdeIndex != mShapeIndex) {
+		mShapeIndex = shapdeIndex;
+		vec3 position = mShapes[mShapeIndex]->getPosition();
+		mHmd->setLookAt(position);
+	}
+}
+
+void VideodrommVRApp::onButtonUp(const ci::vr::Controller::Button *button)
+{
+}
+
+void VideodrommVRApp::update()
+{
+	// Vive sometimes returns the wrong pose data initially so reinitialize the origin matrix after the first 60 frames.
+	if ((ci::vr::API_OPENVR == mVrContext->getApi()) && (!mRecalcOrigin) && (mHmd->getElapsedFrames() > 60)) {
+		mHmd->calculateOriginMatrix();
+		mRecalcOrigin = true;
+	}
+
+	for (auto& shape : mShapes) {
+		bool focused = shape->intersects(mHmd->getInputRay());
+		shape->setFocused(focused);
+	}
+}
+void VideodrommVRApp::cleanup()
+{
+	// save textures
+	VDTexture::writeSettings(mTexs, writeFile(mTexturesFilepath));
+
+	quit();
+}
+void VideodrommVRApp::mouseDown(MouseEvent event)
+{
+	for (auto tex : mTexs)
+	{
+		tex->setXLeft(event.getX());
+		tex->setYTop(event.getY());
+	}
+}
+void VideodrommVRApp::mouseDrag(MouseEvent event)
+{
+	for (auto tex : mTexs)
+	{
+		tex->setXRight(event.getX());
+		tex->setYBottom(event.getY());
+	}
+}
+void VideodrommVRApp::drawScene()
+{
+	gl::enableDepthRead();
+	gl::enableDepthWrite();
+
+	// Draw ground grid
+	{
+		gl::ScopedLineWidth scopedLineWidth(1.0f);
+
+		gl::color(0.8f, 0.8f, 0.8f);
+		mGridBox->draw();
+
+		gl::color(0.65f, 0.65f, 0.65f);
+		mGridLines->draw();
+	}
+
+	// Draw shape
+	for (auto& shape : mShapes) {
+		shape->draw();
+	}
+
+	// Draw coordinate frame
+	{
+		gl::drawCoordinateFrame(1.0f);
+	}
+
+	// Draw controller input rays
+	{
+		gl::lineWidth(3.0f);
+
+		float s = 1000.0f;
+
+		if (mController1 && mController1->hasInputRay()) {
+			const auto& ir = mController1->getInputRay();
+			vec3 dir = ir.getDirection();
+			vec3 orig = ir.getOrigin() + (0.055f * dir);
+			gl::color(0.9f, 0.5f, 0.0f);
+			gl::drawLine(orig, orig + (s * dir));
+		}
+
+		if (mController2 && mController2->hasInputRay()) {
+			const auto& ir = mController2->getInputRay();
+			vec3 dir = ir.getDirection();
+			vec3 orig = ir.getOrigin() + (0.055f * dir);
+			gl::color(0.9f, 0.5f, 0.0f);
+			gl::drawLine(orig, orig + (s * dir));
+		}
+	}
+
+	// Draw HMD input ray (cyan sphere)
+	{
+		const auto& ir = mHmd->getInputRay();
+		vec3 dir = ir.getDirection();
+		vec3 P = ir.getOrigin() + (5.0f * dir);
+		gl::color(0.01f, 0.7f, 0.9f);
+		gl::drawSphere(P, 0.04f);
+	}
+}
+
+void VideodrommVRApp::draw()
+{
+	gl::clear( Color::black() );
+	i = 0;
+	for (auto tex : mTexs)
+	{
+		int x = 128 * i;
+		gl::draw(tex->getTexture(), Rectf(0 + x, 0, 128 + x, 128));
+		i++;
+	}
+	if (mHmd) {
+		mHmd->bind();
+		for (auto eye : mHmd->getEyes()) {
+			mHmd->enableEye(eye);
+			drawScene();
+			mHmd->drawControllers(eye);
+		}
+		mHmd->unbind();
+
+		// Draw mirrored
+		if (mCyclopsMirroring) {
+			mHmd->submitFrame();
+			gl::viewport(getWindowSize());
+			mHmd->enableEye(ci::vr::EYE_HMD);
+			drawScene();
+		}
+		else {
+			gl::viewport(getWindowSize());
+			gl::setMatricesWindow(getWindowSize());
+			mHmd->drawMirrored(getWindowBounds(), true);
+		}
+	}
+	else {
+		gl::viewport(getWindowSize());
+		gl::setMatrices(mCamera);
+		drawScene();
+	}
+}
+void prepareSettings(App::Settings *settings)
+{
+	settings->setTitle("Cinder VR - Videodromm");
+	settings->setWindowSize(1920 / 2, 1080 / 2);
+}
+
+CINDER_APP(VideodrommVRApp, RendererGl(RendererGl::Options().msaa(0)), prepareSettings)
+
